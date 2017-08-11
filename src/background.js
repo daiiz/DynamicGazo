ecdb40754b8d22acfe14d53737b18310dcfaa530
daiiz <da.iz.mg@gmail.com>
2017-08-11 14:28:54 +0900

    フルサイズ画像を保存するように変更

--- src/background.js
+++ src/background.js
@@ -60,12 +60,11 @@
   };
 
   // スクリーンショットをアップロードする
-  var uploader = (svgtag, svgBgBase64Img) => {
+  var uploader = (svgtag, svgBgBase64Img, devicePixelRatio) => {
     SITE_TITLE = svgtag.getAttribute('data-title') || '';
     SITE_URL = svgtag.getAttribute('data-url') || '';
-
     // Ajaxでapi/uploadsvgをたたく
-    console.info(APP_NAME);
+    console.info(APP_NAME, devicePixelRatio);
     $.ajax({
       url: SVGSCREENSHOT_APP + '/api/uploadsvg',
       type: 'POST',
@@ -79,6 +78,7 @@
         viewbox: svgtag.getAttribute('viewBox'),
         public: 'yes',
         gyazo: 'yes',
+        dpr: devicePixelRatio,
         app_name: APP_NAME
       })
     }).success (data => {
@@ -103,37 +103,42 @@
   };
 
   // Canvasに画像をセットして，必要部分のみ切り出す
-  var renderImage = function (linkdata, base64img) {
+  var renderImage = function (linkdata, base64img, devicePixelRatio) {
+    var rat = devicePixelRatio;
     var canvas = document.querySelector("#cav");
     var pos_cropper = linkdata.cropperRect;
     var baseUri = linkdata.baseUri;
     var title = linkdata.title;
     var w = +pos_cropper.width;
     var h = +pos_cropper.height;
-    canvas.width  = w;
-    canvas.height = h;
+    canvas.width  = rat * w;
+    canvas.height = rat * h;
 
     var ctx = canvas.getContext('2d');
-    
-    // MacBook ProのRetinaディスプレイなどの高解像度な
-    // ディスプレイを使用している場合は1より大きな値となる
-    var rat = window.devicePixelRatio;
-    if (rat < 1) rat = 1;
-    ctx.scale(1 / rat, 1 / rat);
-
     var img = new Image();
     img.onload = function () {
       ctx.drawImage(img, rat * pos_cropper.orgX, rat * pos_cropper.orgY, rat * w, rat * h,
-        0, 0, rat * w, rat * h);
+       0, 0, rat * w, rat * h);
+      // ctx.drawImage(img, pos_cropper.orgX, pos_cropper.orgY, w, h,
+      //   0, 0, w, h);
       var screenshot = canvas.toDataURL('image/png');
       // SVGスクリーンショットタグをつくる
-      makeSVGtag(linkdata.aTagRects, linkdata.text, screenshot, w, h, baseUri, title);
+      makeSVGtag(
+        linkdata.aTagRects,
+        linkdata.text,
+        screenshot,
+        w,
+        h,
+        baseUri,
+        title,
+        rat);
     };
     img.src = base64img;
   };
 
   // SVGタグを生成する
-  var makeSVGtag = function (aTagRects, text, base64img, width, height, baseUri, title) {
+  var makeSVGtag = function (
+    aTagRects, text, base64img, width, height, baseUri, title, devicePixelRatio) {
     var svgns  = 'http://www.w3.org/2000/svg';
     var hrefns = 'http://www.w3.org/1999/xlink';
     // root SVG element
@@ -191,7 +196,7 @@
     rootSVGtag.setAttributeNS(null, 'data-title', validateTitle(title));
 
     // スクリーンショットをアップロード
-    uploader(rootSVGtag, base64img);
+    uploader(rootSVGtag, base64img, devicePixelRatio);
   };
 
   // ポップアップ画面から命令を受ける
@@ -205,7 +210,7 @@
         MODE = opts.mode;
         APP_NAME = opts.app || '';
         SCRAP_BOX_ID = opts.scrapbox_box_id;
-        renderImage(linkdata, dataUrl);
+        renderImage(linkdata, dataUrl, opts.dpr);
       });
     }else if (request.command === 'get-scrapbox-list') {
       // scrapboxボックス名リストを返す
